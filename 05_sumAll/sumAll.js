403c203f46113b03df8d4bae4099011504a5d3fb
Jack Alessi <jackalessi7@gmail.com>
2025-03-27 00:10:49 -0400

    Solve exercises 1 through 7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if (
+        typeof a !== 'number' ||
+        typeof b !== 'number' ||
+        a < 0 ||
+        b < 0 ||
+        !Number.isInteger(a) ||
+        !Number.isInteger(b)
+    ) {return "ERROR";
+    }
+        let start = Math.min(a,b);
+        let end = Math.max(a,b);
+        let sum = 0;
 
+        for (let i = start; i <= end; i++) {
+            sum += i;
+        }
+
+        return sum;
 };
 
 // Do not edit below this line
